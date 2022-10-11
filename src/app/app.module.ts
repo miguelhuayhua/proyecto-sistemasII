ce355bdd7ea1d60f05d4d5abc319f7ddcdd4738f
Miguel Huayhua <miguelhuayhuac2@gmail.com>
2022-10-11 11:55:31 -0400

    Mejorado responsive del login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginModule } from './login-module/login-module.module';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './dashboard-module/dashboard.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +18,12 @@ import { DashboardModule } from './dashboard-module/dashboard.module';
     NgbModule,
     LoginModule,
     DashboardModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
